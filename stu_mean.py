34fb33da6d9fb070eb9346d52eb8584b05f209c3
aroktim@stuy.edu <aroktim@stuy.edu>
2017-10-17 13:20:54 -0400

    Modularize

--- stu_mean.py
+++ stu_mean.py
@@ -7,28 +7,33 @@ f="discobandit.db"
 db = sqlite3.connect(f) # open if f exists, otherwise create
 c = db.cursor()    # facilitate db ops
 
-# get the ids
-command = "SELECT id FROM peeps"
-identifiers = c.execute(command)
+def find_student( id ):
+    print "===============\n", id[0]
+    command = "SELECT mark FROM courses WHERE " + str(id[0]) + " = id"
+    student = c.execute(command)
+    return student
 
-for stu in identifiers.fetchall():
-    #print "Student:", stu[0]
-    # Get the marks from courses where the id is equal to stu
-    grades = c.execute("SELECT mark FROM courses WHERE " + str(stu[0]) + " = id")
-    #print grades
-    # calculate the average for the student
-    average = length = 0
-    for mark in grades:
-        #print "Mark for " + str(stu[0]) + ": " + str(mark[0])
-        average += mark[0]
-        length += 1
-    average /= length
-    #print "Average for Student " + str(stu[0]) + ": " + str(average)
-    # get the name of the student from their id
-    names = c.execute("SELECT name FROM peeps WHERE " + str(stu[0]) + " = id")
-    # Print student name, their id, and average
-    for name in names.fetchall():
-        print "Average for student " + name[0] + " who has id " +\
-                str(stu[0]) + " is " + str(average)
+def print_student():
+    # get the ids
+    command = "SELECT id FROM peeps"
+    ids = c.execute(command)
 
+    for student in ids.fetchall():
+        grades = find_student(student)
+        for grade in grades:
+            print grade[0]
 
+def calc_avg():
+    command = "SELECT id FROM peeps"
+    ids = c.execute(command)
+    for student in ids.fetchall():
+        grades = find_student(student)
+        average = number = 0
+        for grade in grades:
+            average += grade[0]
+            number += 1
+        average /= number
+        print "Average for student id " + str(student[0]) + " is: " + str(average)
+            
+print_student()
+calc_avg()

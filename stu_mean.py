a496776fd9f855fbae16606fbf403a44e02f1cd3
Arif Roktim <aroktim@stuy.edu>
2017-10-16 21:13:52 -0400

    Add comments

--- stu_mean.py
+++ stu_mean.py
@@ -1,22 +1,22 @@
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
+import sqlite3   # enable control of an sqlite database
+import csv       # facilitates CSV I/O
 
 
 f="discobandit.db"
 
-db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
+db = sqlite3.connect(f) # open if f exists, otherwise create
+c = db.cursor()    # facilitate db ops
 
+# get the ids
 command = "SELECT id FROM peeps"
 identifiers = c.execute(command)
-#print identifiers.fetchall()
-#print identifiers.fetchall()
-
 
 for stu in identifiers.fetchall():
     #print "Student:", stu[0]
+    # Get the marks from courses where the id is equal to stu
     grades = c.execute("SELECT mark FROM courses WHERE " + str(stu[0]) + " = id")
     #print grades
+    # calculate the average for the student
     average = length = 0
     for mark in grades:
         #print "Mark for " + str(stu[0]) + ": " + str(mark[0])
@@ -24,8 +24,10 @@ for stu in identifiers.fetchall():
         length += 1
     average /= length
     #print "Average for Student " + str(stu[0]) + ": " + str(average)
+    # get the name of the student from their id
     names = c.execute("SELECT name FROM peeps WHERE " + str(stu[0]) + " = id")
-    for name in names:
+    # Print student name, their id, and average
+    for name in names.fetchall():
         print "Average for student " + name[0] + " who has id " +\
                 str(stu[0]) + " is " + str(average)
 
